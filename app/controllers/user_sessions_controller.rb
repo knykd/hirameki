2302cab784f604eb2098c087c68d48a5837a20fd
knykd <kok1022kok@gmail.com>
2020-01-21 19:42:35 +0900

    flashメッセージ表示機能追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,14 +5,15 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to(root_path)
+      redirect_to root_path, success: t('user_sessions.flash.create.success')
     else
+      flash.now[:danger] = t 'user_sessions.flash.create.danger'
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to(root_path)
+    redirect_to root_path, success: t('user_sessions.flash.destroy.success')
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, success: t('users.flash.create.success')
     else
+      flash.now[:danger] = t 'users.flash.create.danger'
       render :new
     end
   end
