045d11cdef8b84f7f8f02c2165fec3b1f527d178
knykd <kok1022kok@gmail.com>
2020-01-21 19:59:59 +0900

    wordsテーブルの追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
+  resources :words, only: %i[index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tops#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_031254) do
+ActiveRecord::Schema.define(version: 2020_01_21_105354) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2020_01_21_031254) do
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "words", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "word_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
