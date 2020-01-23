22b600f9cc0f5ddd659534574cd60d1aeac1dc8d
knykd <kok1022kok@gmail.com>
2020-01-23 18:18:45 +0900

    ログインパスワードにnull: false設定

--- db/migrate/20200121030541_sorcery_core.rb
+++ db/migrate/20200121030541_sorcery_core.rb
@@ -2,7 +2,7 @@ class SorceryCore < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :email, null: false
-      t.string :crypted_password
+      t.string :crypted_password, null: false
       t.string :salt
       t.string :name, null: false
 
