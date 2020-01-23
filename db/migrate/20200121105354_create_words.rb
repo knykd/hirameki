3ab006f4e821746657677df056b7d4478b9daa10
knykd <kok1022kok@gmail.com>
2020-01-23 18:20:05 +0900

    Wordテーブルのカラム名変更

--- db/migrate/20200121105354_create_words.rb
+++ db/migrate/20200121105354_create_words.rb
@@ -1,7 +1,7 @@
 class CreateWords < ActiveRecord::Migration[5.2]
   def change
     create_table :words do |t|
-      t.string :word_name, null: false
+      t.string :name, null: false
 
       t.timestamps
     end
